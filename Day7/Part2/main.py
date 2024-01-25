bb03dc03a6acc4938eb63914cdf4b8cd2f473e10
trevorgrabham <tgrabham@sfu.ca>
2024-01-24 16:59:44 -0800

    Finished part 2 and cleaned part 1

--- Day7/Part2/main.py
+++ Day7/Part2/main.py
@@ -1,10 +1,11 @@
 import re
+import traceback
 from functools import total_ordering
 
 class Hand:
 
   typeRankings = {'FiveOfAKind': 7, 'FourOfAKind': 6, 'FullHouse': 5, 'ThreeOfAKind': 4, 'TwoPair': 3, 'OnePair': 2, 'HighCard': 1}
-  cardRankings = {'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8, '8': 7, '7': 6, '6':5, '5': 4, '4': 3, '3': 2, '2': 1}
+  cardRankings = {'A': 13, 'K': 12, 'Q': 11, 'J': 1, 'T': 10, '9': 9, '8': 8, '7': 7, '6':6, '5': 5, '4': 4, '3': 3, '2': 2}
 
   def __eq__(self, other):
     for s, o in zip(self.cardOrdering, other.cardOrdering):
@@ -67,42 +68,58 @@ class Hand:
     return f'Hand: {self.cards}\nBet: {self.bet}\nType: {self.type}'
 
   def checkFiveOfAKind(self):
-    if self.cardOrdering[0] == self.cardOrdering[4]:
+    if self.numberOfJacks == 5:
+      self.type = 'FiveOfAKind'
+      return
+    if self.cardOrdering[0] == self.cardOrdering[4-self.numberOfJacks]:
       self.type = 'FiveOfAKind'
       return
     self.checkFourOfAKind()
   
   def checkFourOfAKind(self):
-    if self.cardOrdering[0] == self.cardOrdering[3]:
-      self.type = 'FourOfAKind'
-      return
-    elif self.cardOrdering[1] == self.cardOrdering[4]:
+    if self.cardOrdering[0] == self.cardOrdering[4-self.numberOfJacks-1] or self.cardOrdering[1] == self.cardOrdering[4-self.numberOfJacks]:
       self.type = 'FourOfAKind'
       return
     self.checkFullHouse()
 
   def checkFullHouse(self):
-    if (self.cardOrdering[0] == self.cardOrdering[1] and self.cardOrdering[0] == self.cardOrdering[2]):
-      if self.cardOrdering[3] == self.cardOrdering[4]:
-        self.type = 'FullHouse'
+    if self.numberOfJacks == 0:
+      if self.cardOrdering[0] == self.cardOrdering[1] and self.cardOrdering[0] == self.cardOrdering[2]:
+        if self.cardOrdering[3] == self.cardOrdering[4]:
+          self.type = 'FullHouse'
+        else:
+          self.type = 'ThreeOfAKind'
+      elif self.cardOrdering[2] == self.cardOrdering[3] and self.cardOrdering[2] == self.cardOrdering[4]:
+        if self.cardOrdering[0] == self.cardOrdering[1]:
+          self.type = 'FullHouse'
+        else:
+          self.type = 'ThreeOfAKind'
       else:
-        self.type = 'ThreeOfAKind'
-    elif self.cardOrdering[2] == self.cardOrdering[3] and self.cardOrdering[2] == self.cardOrdering[4]:
-      if self.cardOrdering[0] == self.cardOrdering[1]:
+        self.checkThreeOfAKind()
+    elif self.numberOfJacks == 1:
+      if self.cardOrdering[0] == self.cardOrdering[1] and self.cardOrdering[2] == self.cardOrdering[3]:
         self.type = 'FullHouse'
-      else:
-        self.type = 'ThreeOfAKind'
-    else:
-      self.checkThreeOfAKind()
+      else: 
+        self.checkThreeOfAKind()
+    else: # must be two 'J' cards, otherwise if there were 3 or more, we would be guaranteed a four or five of a kind
+      self.type = 'ThreeOfAKind'
   
   def checkThreeOfAKind(self):
+    if self.numberOfJacks == 1:
+      if self.cardOrdering[0] == self.cardOrdering[1] or self.cardOrdering[1] == self.cardOrdering[2] or self.cardOrdering[2] == self.cardOrdering[3]:
+        self.type = 'ThreeOfAKind'
+        return
+      else:
+        self.type = 'OnePair'
+        return
     i = 0
     while i < 3:
       if self.cardOrdering[i] == self.cardOrdering[i+1] and self.cardOrdering[i] == self.cardOrdering[i+2]:
         self.type = 'ThreeOfAKind'
         return
       i += 1
-    self.checkPairOrTwoPair()
+    else:
+      self.checkPairOrTwoPair()
   
   def checkPairOrTwoPair(self):
     i = 0
@@ -125,7 +142,8 @@ class Hand:
     self.type = None 
     self.bet = bet
     self.cards = [card for card in cardString]
-    self.cardOrdering = [Hand.cardRankings[card] for card in cardString]
+    self.numberOfJacks = len([card for card in cardString if card == 'J'])
+    self.cardOrdering = [Hand.cardRankings[card] for card in cardString if card != 'J']
     self.cardOrdering.sort(reverse = True)
 
     self.checkFiveOfAKind()
@@ -149,6 +167,7 @@ def main():
     print("Error: File 'input.txt' not found")
   except Exception as e:
     print(f"Error: {e}")
+    traceback.print_exc()
 
 if __name__ == "__main__":
   main()
